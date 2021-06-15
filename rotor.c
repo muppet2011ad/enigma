a109b399ea6158ea04d30682a60553121a7c0a9e
James Townsend <j.a.townsend2000@gmail.com>
2021-06-15 17:17:11 +0100

    Trimmed unnecessary functions from rotor.c

--- rotor.c
+++ rotor.c
@@ -2,14 +2,13 @@
 #include "rotor.h"
 #include <string.h>
 
-int rotate_rotor(rotor r) {
+int r_rotate(rotor r) {
     int special = 0; // Determines whether rotor 2 rotates if this is rotor 1 or we double step if this is rotor 2
     // Behaviour is implemented by whatever calls this
-    if (!r->just_moved && r->slot == 0 && r->notch == r->step) {
+    if (r->notch == r->step) {
         special = 1;
     }
     r->step = (r->step + 1) % 26;
-    r->just_moved = 1;
     return special;
 }
 
@@ -17,16 +16,10 @@ char r_sub (rotor r, char c, int reflected) { // Performs substitution in both d
     return r->substitutions[((c - 'A' + r->step) % 26) + reflected*26];
 }
 
-void r_end_keypress(rotor r) { // Resets rotor movement
-    r->just_moved = 0;
-}
-
-rotor create_rotor(char* subs, char notch, unsigned short start_pos, unsigned short slot) { // Creates a rotor
+rotor create_rotor(char* subs, char notch, char start_pos) { // Creates a rotor
     rotor r = malloc(sizeof(struct rotor_structure)); // TODO: error checking on malloc
     r->notch = notch - 'A';
-    r->step = start_pos;
-    r->slot = slot;
-    r->just_moved = 0;
+    r->step = start_pos - 'A';
     strncpy(r->substitutions, subs, 26); // Copies substitutions in
     for (int i = 0; i < 26; i++) { // Calculates substitutions in the reflected direction (quicker than working it out for every keypress)
         char reverse_input = r->substitutions[i];
@@ -34,4 +27,8 @@ rotor create_rotor(char* subs, char notch, unsigned short start_pos, unsigned sh
     }
 
     return r;
+}
+
+char r_get_position(rotor r) {
+    return r->step + 'A';
 }
--- rotor.h
+++ rotor.h
@@ -8,15 +8,13 @@ struct rotor_structure {
     char substitutions[52]; // Stores normal substitutions in positions 0-25 and reflections in 26-51
     unsigned short notch : 5; // Notch position
     unsigned short step : 5; // Current rotation step
-    unsigned short slot : 2; // Which slot the rotor is in
-    unsigned short just_moved : 1; // Determines whether the rotor has moved this keypress (relevant for double-stepping)
 };
 
 typedef struct rotor_structure *rotor;
 
-rotor create_rotor(char* subs, char notch, unsigned short start_pos, unsigned short slot);
-void r_end_keypress(rotor r);
+rotor create_rotor(char* subs, char notch, char start_pos);
 char r_sub (rotor r, char c, int reflected);
-int rotate_rotor(rotor r);
+int r_rotate(rotor r);
+char r_get_position(rotor r);
 
 #endif
