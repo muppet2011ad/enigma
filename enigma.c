aeb288871dbd15bf7f5fd557840f9abbd6176063
James Townsend <j.a.townsend2000@gmail.com>
2021-06-15 17:44:51 +0100

    Better loading and organisation of rotor data

--- enigma.c
+++ enigma.c
@@ -13,19 +13,30 @@ int main() {
     read_lines(file, &lines, &num_lines, &lines_arr_size);
     fclose(file);
 
+    char rotor_subs[num_lines][26];
+    char rotor_notches[num_lines-1];
+
     for (int i = 0; i < num_lines; i++) {
-        printf("%s", lines[i]);
+        char *ptr = strtok(lines[i], " ");
+        strncpy(rotor_subs[i], ptr, 26);
+        if (i != num_lines-1) {
+            ptr = strtok(NULL, " ");
+            rotor_notches[i] = *ptr;
+        }
+        else {
+            rotor_notches[i] = 26;
+        }
+        free(lines[i]);
     }
+    free(lines);
 
-    printf("\n\n");
-
-    rotor r0 = create_rotor(lines[0], 'Q', 'P');
-    rotor r1 = create_rotor(lines[2], 'V', 'U');
-    rotor r2 = create_rotor(lines[4], 'Z', 'H');
+    rotor r0 = create_rotor(rotor_subs[0], rotor_notches[0], 'P');
+    rotor r1 = create_rotor(rotor_subs[2], rotor_notches[2], 'U');
+    rotor r2 = create_rotor(rotor_subs[4], rotor_notches[4], 'H');
 
     rotor rotors[] = {r0, r1, r2};
 
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 10; i++) {
         printf("%c, %c, %c\n", r_get_position(r0), r_get_position(r1), r_get_position(r2));
         keypress_rotate(rotors);
     }
