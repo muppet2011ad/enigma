e1fba9b2a82f115e622fa561a8c0a1c14ee92842
James Townsend <j.a.townsend2000@gmail.com>
2021-06-21 13:44:25 +0100

    Refactoring of setup methods to separate setup and file-handling

--- enigma.c
+++ enigma.c
@@ -16,40 +16,22 @@ void keypress_rotate(rotor rotors[3]);
 char *encode_message(char *message, rotor rotors[3], rotor reflector, char plugboard[26]);
 char p_sub(char c, char p[26]);
 void input(char *string,int length);
-enigma create_engima(FILE *config_file, r_template templates[], int num_templates);
+enigma create_enigma_from_file(FILE *config_file, r_template templates[], int num_templates);
 void destroy_engima(enigma e);
 void display_config(enigma e);
+r_template *load_templates_from_file(FILE* f, int *num_rotors);
+enigma create_enigma(r_template rotors[3], r_template reflector, char positions[3], char ring_settings[3], char plugboard[27], char pairs[30]);
 
 int main() {
-    int num_lines = 0;
-    int lines_arr_size = LINES_ARR_LEN;
-    char **lines = calloc(LINES_ARR_LEN, sizeof(char*));
     FILE* file = fopen("rotors", "r");
-    read_lines(file, &lines, &num_lines, &lines_arr_size);
+    int num_rotors = 0;
+    r_template *templates = load_templates_from_file(file, &num_rotors);
     fclose(file);
 
-    r_template templates[num_lines];
-
-    for (int i = 0; i < num_lines; i++) {
-        char *ptr = strtok(lines[i], " ");
-        strncpy(templates[i].substitutions, ptr, 26);
-        ptr = strtok(NULL, " ");
-        if (i != num_lines-1) {
-            templates[i].notch = *ptr;
-            templates[i].id = i;
-        }
-        else {
-            templates[i].notch = 27;
-            templates[i].id = 0;
-        }
-        free(lines[i]);
-    }
-
-    free(lines);
-
     FILE *config_file = fopen("config", "r");
-    enigma e = create_engima(config_file, templates, num_lines);
+    enigma e = create_enigma_from_file(config_file, templates, num_rotors);
     fclose(config_file);
+    free(templates);
 
     display_config(e);
 
@@ -64,30 +46,66 @@ int main() {
     free(result);
 }
 
-enigma create_engima(FILE *config_file, r_template templates[], int num_templates) {
+r_template *load_templates_from_file(FILE* f, int *num_rotors) {
+    int arr_size = LINES_ARR_LEN;
+    char **lines = calloc(LINES_ARR_LEN, sizeof(char*));
+    read_lines(f, &lines, num_rotors, &arr_size);
+    r_template *templates = malloc(*num_rotors*sizeof(r_template));
+    for (int i = 0; i < *num_rotors; i++) {
+        char *ptr = strtok(lines[i], " ");
+        strncpy(templates[i].substitutions, ptr, 26);
+        ptr = strtok(NULL, " ");
+        if (i != *num_rotors-1) {
+            templates[i].notch = *ptr;
+            templates[i].id = i;
+        }
+        else {
+            templates[i].notch = 27;
+            templates[i].id = 0;
+        }
+        free(lines[i]);
+    }
+
+    free(lines);
+    return templates;
+}
+
+enigma create_enigma(r_template rotors[3], r_template reflector, char positions[3], char ring_settings[3], char plugboard[27], char pairs[30]) {
     enigma e = malloc(sizeof(struct enigma_structure)); // Allocate memory for enigma
+    e->rotors[0] = create_rotor(rotors[0], positions[0], ring_settings[0]);
+    e->rotors[1] = create_rotor(rotors[1], positions[1], ring_settings[1]);
+    e->rotors[2] = create_rotor(rotors[2], positions[2], ring_settings[2]);
+    e->reflector = create_rotor(reflector, 'A', 'A');
+    strcpy(e->plugboard, plugboard);
+    strcpy(e->pairs, pairs);
+    return e;
+}
+
+enigma create_enigma_from_file(FILE *config_file, r_template templates[], int num_templates) {
     int num_config_lines = 0;
     int config_lines_arr_size = LINES_ARR_LEN;
     char **config_lines = calloc(LINES_ARR_LEN, sizeof(char*));
     read_lines(config_file, &config_lines, &num_config_lines, &config_lines_arr_size); // Read in config file
-    e->rotors[0] = create_rotor(templates[config_lines[0][0] - '1'], config_lines[1][0], config_lines[2][0]);
-    e->rotors[1] = create_rotor(templates[config_lines[0][2] - '1'], config_lines[1][2], config_lines[2][2]);
-    e->rotors[2] = create_rotor(templates[config_lines[0][4] - '1'], config_lines[1][4], config_lines[2][4]);
-    e->reflector = create_rotor(templates[num_templates-1], 'A', 'A');
 
-    strcpy(e->plugboard, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"); // Copy in identity plugboard
+    char plugboard[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\0";
+    char pairs[30] = "\0";
     // Loops through steckered pairs given in config
     if (config_lines[3] != NULL) {
-        strncpy(e->pairs, config_lines[3], 30);
-        e->pairs[29] = '\0';
+        strncpy(pairs, config_lines[3], 30);
+        pairs[29] = '\0';
         char *ptr = strtok(config_lines[3], " ");
         while (ptr != NULL) {
-            e->plugboard[ptr[0] - 'A'] = ptr[1];
-            e->plugboard[ptr[1] - 'A'] = ptr[0];
+            plugboard[ptr[0] - 'A'] = ptr[1];
+            plugboard[ptr[1] - 'A'] = ptr[0];
             ptr = strtok(NULL, " ");
         }
     }
-    else { e->pairs[0] = '\0'; }
+
+    r_template rotors[3] = {templates[config_lines[0][0] - '1'], templates[config_lines[0][2] - '1'], templates[config_lines[0][4] - '1']};
+    char positions[3] = {config_lines[1][0], config_lines[1][2], config_lines[1][4]};
+    char ring_settings[3] = {config_lines[2][0], config_lines[2][2], config_lines[2][4]};
+
+    enigma e = create_enigma(rotors, templates[num_templates-1], positions, ring_settings, plugboard, pairs);
 
     for (int i = 0; i < num_config_lines; i++) {
         free(config_lines[i]);
