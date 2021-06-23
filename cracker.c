7f565966d311a232d7995e9e22a7cdcd113933c2
James Townsend <j.a.townsend2000@gmail.com>
2021-06-23 18:34:28 +0100

    Cracking vaguely works for rotor selection and positioning

--- cracker.c
+++ cracker.c
@@ -2,20 +2,112 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 #include "enigma.h"
+
+#define IOC_ENGLISH_TEXT 1.73
+#define ROTORS 5
+#define BEST_ROTORS_COUNT 10
+
+struct rotor_config_and_score_structure {
+    int rotors[3];
+    char positions[3];
+    char ring_settings[3];
+    char plugboard[27];
+    char pairs[30];
+    double score;
+};
+
+typedef struct rotor_config_and_score_structure rotor_config_and_score;
 
 double ioc(char *text);
 char *read_in_line(FILE *input_file);
+void handle_new_score(rotor_config_and_score *best_list, int list_length, rotor_config_and_score new_score);
 
 int main() {
     int num_rotors = 0;
     FILE* templates_file = fopen("rotors", "r");
     r_template *templates = load_templates_from_file(templates_file, &num_rotors);
     fclose(templates_file);
-    FILE* source_file = fopen("secret", "r");
+    FILE* source_file = fopen("example", "r");
     char *contents = read_in_line(source_file);
-    printf("%s\n%f\n", contents, ioc(contents));
+    fclose(source_file);
+
+    //printf("%s\n", contents);
+    
+    //double *scores[5][5][26][26][26] = calloc(5*5*5*26*26*26, sizeof(double));
+    rotor_config_and_score *scores = calloc(BEST_ROTORS_COUNT, sizeof(rotor_config_and_score));
+
+    char r_id_lookup[8][5] = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII"};
+    char ring_settings[3] = "AAA";
+    char plugboard[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char pairs[30] = "\0";
+    for (int i = 0; i < num_rotors-1; i++) {
+        for (int j = 0; j < num_rotors-1; j++) {
+            if (i == j) { continue; }
+            for (int k = 0; k < num_rotors-1; k++) {
+                if (j == k || i == k) { continue; }
+                printf("%5s %5s %5s\n", r_id_lookup[i], r_id_lookup[j], r_id_lookup[k]);
+                double numerator = 0;
+                for (int x = 0; x < 26; x++) {
+                    for (int y = 0; y < 26; y++) {
+                        for (int z = 0; z < 26; z++) {
+                            if (i == j || j == k || i == k) {
+                                //scores[i][j][k][x][y][z] = -1;
+                            }
+                            else {
+                                r_template rotors[3] = {templates[i], templates[j], templates[k]};
+                                char positions[3] = {x + 'A', y + 'A', z + 'A'};
+                                enigma e = create_enigma(rotors, templates[num_rotors-1], positions, ring_settings, plugboard, pairs);
+                                char *result = encode_message(contents, e);
+                                double score = ioc(result);
+                                //scores[i][j][k][x][y][z] = score;
+                                numerator += score;
+                                free(result);
+                                rotor_config_and_score rs;
+                                rs.rotors[0] = i;
+                                rs.rotors[1] = j;
+                                rs.rotors[2] = k;
+                                strncpy(rs.ring_settings, "AAA", 3);
+                                strncpy(rs.positions, positions, 3);
+                                strcpy(rs.plugboard, plugboard);
+                                strcpy(rs.pairs, pairs);
+                                rs.score = score;
+                                handle_new_score(scores, BEST_ROTORS_COUNT, rs);
+                                destroy_engima(e);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < BEST_ROTORS_COUNT; i++) {
+        printf("%2d: %5s %5s %5s in position %c %c %c with score %f\n\n", i+1, r_id_lookup[scores[i].rotors[0]], r_id_lookup[scores[i].rotors[1]], r_id_lookup[scores[i].rotors[2]], scores[i].positions[0], scores[i].positions[1], scores[i].positions[2], scores[i].score);
+    }
+
     free(contents);
+    free(templates);
+}
+
+void handle_new_score(rotor_config_and_score *best_list, int list_length, rotor_config_and_score new_score) {
+    for (int i = list_length; i >= 0; --i) {
+        if (fabs(new_score.score - IOC_ENGLISH_TEXT) < fabs(best_list[i].score - IOC_ENGLISH_TEXT)) {
+            if (i != list_length-1) {
+                best_list[i+1] = best_list[i];
+            }
+            if (i == 0) {
+                best_list[0] = new_score;
+            }
+            else if (!(fabs(new_score.score - IOC_ENGLISH_TEXT) < fabs(best_list[i-1].score - IOC_ENGLISH_TEXT))) {
+                best_list[i] = new_score;
+            }
+        }
+        else {
+            break;
+        }
+    }
 }
 
 double ioc(char *text) {
